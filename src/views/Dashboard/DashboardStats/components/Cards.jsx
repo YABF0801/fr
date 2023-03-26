15024e183a422e69ff15cd262d837f74e6c97b81
YABF0801 <yabf0801@gmail.com>
2023-03-26 19:32:22 -0400

    fiz cards estadisticas

--- src/views/Dashboard/DashboardStats/components/Cards.jsx
+++ src/views/Dashboard/DashboardStats/components/Cards.jsx
@@ -1,39 +1,80 @@
+
+import { useEffect, useState } from 'react';
+
 import './Stats.scss';
+import { getCapacityAndMatricula, getTotalGirlsAndBoys } from '../service/dashboard.services';
 
 const Cards = () => {
+    const [totalMatricula, setTotalMatricula] = useState(0);
+    const [totalCapacidad, setTotalCapacidad] = useState(0);
+    const [totalBoys, setTotalBoys] = useState(0);
+    const [totalGirls, setTotalGirls] = useState(0);
+   
+    useEffect(() => {
+        const fetchData = async () => {
+          const result = await getCapacityAndMatricula();
+          setTotalCapacidad(result.NormedCapacity);
+          setTotalMatricula(result.Matricula);
+        };
+        fetchData();
+      }, []);
+
+      useEffect(() => {
+        const fetchData = async () => {
+          const result = await getTotalGirlsAndBoys();
+          setTotalBoys(result.totalBoys);
+          setTotalGirls(result.totalGirls);
+        };
+        fetchData();
+      }, []);
+
+
 	return (
         <section className='estadisticas'>
+
         <div className='container-main mt-3 p-3'>
             
             <div className='row mt-3 justify-content-evenly'>
-                <div className='col-md-4 col-xl-4'>
+                <div className='col-md-3 col-xl-3'>
                     <div className='card bg-c-yellow order-card'>
                         <div className='card-block'>
                             <h5 className='m-b-20'>Total de Matriculados</h5>
                             <h1 className='text-right display-1'>
-                                <span>2287</span>
+                                <span>{totalMatricula}</span>
                             </h1>
                         </div>
                     </div>
                 </div>
 
-                <div className='col-md-4 col-xl-4'>
+                <div className='col-md-3 col-xl-3'>
                     <div className='card bg-c-pink order-card'>
+                        <div className='card-block'>
+                            <h5 className='m-b-20'>Total de Capacidades</h5>
+                            <h1 className='text-right display-1'>
+                                <span>{totalCapacidad}</span>
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-md-3 col-xl-3'>
+                    <div className='card bg-c-green order-card'>
                         <div className='card-block'>
                             <h5 className='m-b-20'>Total de Niñas</h5>
                             <h1 className='text-right display-1'>
-                                <span>1092</span>
+                                <span>{totalGirls}</span>
                             </h1>
                         </div>
                     </div>
                 </div>
 
-                <div className='col-md-4 col-xl-4'>
-                    <div className='card bg-c-green order-card'>
+                
+                <div className='col-md-3 col-xl-3'>
+                    <div className='card bg-c-blue order-card'>
                         <div className='card-block'>
                             <h5 className='m-b-20'>Total de Niños</h5>
                             <h1 className='text-right display-1'>
-                                <span>1195</span>
+                                <span>{totalBoys}</span>
                             </h1>
                         </div>
                     </div>
