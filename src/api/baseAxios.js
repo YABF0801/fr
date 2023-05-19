591f778a167a1b6027f1ab1d11cb2702875454db
Yoan Estrada Blanco <45434999+yoan0102@users.noreply.github.com>
2023-05-19 19:21:51 -0400

    fix: agregando bien el token a el interceptor

--- src/api/baseAxios.js
+++ src/api/baseAxios.js
@@ -2,5 +2,16 @@ import axios from 'axios';
 
 export const baseAxios = axios.create({
 	/* baseURL: 'http://localhost:8000/api/v1', */
-	baseURL: 'http://127.0.0.1:8000/api/v1',
+	baseURL: 'http://localhost:5000/api/v1',
+});
+
+
+
+
+baseAxios.interceptors.request.use(config => {
+	const { token } = JSON.parse(localStorage.getItem('MY_AUTH_APP_1'));
+	if (token) {
+		config.headers.authorization = `Bearer ${token}`;
+	}
+	return config;
 });
