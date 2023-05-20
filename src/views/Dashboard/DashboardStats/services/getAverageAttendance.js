761d407ba9a55db8e2e456218c67ad1e60f81765
YABF0801 <yabf0801@gmail.com>
2023-05-19 22:49:34 -0400

    separar service de getAverageAttendance

---
+++ src/views/Dashboard/DashboardStats/services/getAverageAttendance.js
@@ -0,0 +1,15 @@
+import { baseAxios } from '../../../../api/baseAxios';
+
+export const getAverageAttendance = async () => {
+	// porcientos de asistencia
+	const result = await baseAxios.get('/estadisticas/asistencia');
+	const attendanceData = result.data[0];
+	const data = [
+		attendanceData.totalAttendance2 || 50, // remove numbers
+		attendanceData.totalAttendance3 || 25,
+		attendanceData.totalAttendance4 || 80,
+		attendanceData.totalAttendance5 || 10,
+		attendanceData.totalAttendance6 || 100,
+	];
+	return data;
+};
