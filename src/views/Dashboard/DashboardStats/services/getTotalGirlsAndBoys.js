c730a0b8f57cfbf2f90b0ed765a0307c473a24be
YABF0801 <yabf0801@gmail.com>
2023-05-19 22:52:45 -0400

    separar service de getTotalGirlsAndBoys

--- src/views/Dashboard/DashboardStats/services/getTotalGirlsAndBoys.js
+++ src/views/Dashboard/DashboardStats/services/getTotalGirlsAndBoys.js
@@ -1,7 +1,7 @@
 import { baseAxios } from '../../../../api/baseAxios';
 
 export const getTotalGirlsAndBoys = async () => {
-	//  TOTAL NIÑOS POR AÑO
+	//  TOTAL NIÑOS
 	const totalBoysGirls = await baseAxios.get('/estadisticas/boys-girls');
 	return totalBoysGirls.data;
 };
