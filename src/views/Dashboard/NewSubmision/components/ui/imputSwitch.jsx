7fc63dc03d86e5bc76a794dd4c59f0f8bf64b6d6
YABF0801 <yabf0801@gmail.com>
2023-05-23 19:23:27 -0400

    crear un componente para switch input para forms

---
+++ src/views/Dashboard/NewSubmision/components/ui/imputSwitch.jsx
@@ -0,0 +1,28 @@
+
+
+const InputSwitch = ({ className, id, name, value, label, onChange, onBlur, defaultChecked}) => {
+  return (
+    <div className={className || 'col-md-2 mb-4 form-check form-switch'}>
+    <input
+      className='form-check-input'
+      type='checkbox'
+      id= {id}
+      name={name}
+      value={value}
+      onChange={ onChange }
+      onBlur={onBlur }
+      defaultChecked ={defaultChecked || false}
+    />
+
+    <label
+      htmlFor={id}
+    >
+      {label}
+    </label>
+
+</div>
+
+  )
+}
+
+export default InputSwitch
