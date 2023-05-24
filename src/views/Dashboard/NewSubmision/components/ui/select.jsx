83113b491c969e0fe4b23a022d1d5ce7c93b10e6
YABF0801 <yabf0801@gmail.com>
2023-05-23 20:21:00 -0400

    crear componente select para los forms

---
+++ src/views/Dashboard/NewSubmision/components/ui/select.jsx
@@ -0,0 +1,29 @@
+
+const Select = ({ className, id, name, value, label, optionText, onChange, onBlur, disabled, mapFunction}) => {
+	return (
+    <div className={className ||'form-group col-md-12 mb-3 d-flex'}>
+      <label 
+          className='col-md-6 m-md-2 text-secondary' 
+          htmlFor={id}
+      >
+          {label}
+      </label>
+
+			<select 
+          className='form-control' 
+          id={id} 
+          name={name} 
+          value={value} 
+          onChange={onChange} 
+          onBlur={onBlur}
+          disabled ={disabled || false}
+        >
+				<option className='text-center'>{optionText}</option>
+				{mapFunction}
+			</select>
+		</div>
+	);
+};
+
+export default Select;
+
