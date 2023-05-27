81645f25219493e21ca9ca9f5bd96e914475e88b
YABF0801 <yabf0801@gmail.com>
2023-05-27 03:29:29 -0400

    organizar submision form grid

--- src/views/Dashboard/NewSubmision/components/SubmisionForm.jsx
+++ src/views/Dashboard/NewSubmision/components/SubmisionForm.jsx
@@ -6,7 +6,6 @@ import { circulosApiGet } from '../../Circulos/service/circulo.services';
 import InputSwitch from '../../../../common/uiForms/imputSwitch';
 import Select from '../../../../common/uiForms/select';
 
-
 const SubmisionForm = ({ form, submision }) => {
 	const [newEntryNumber, setNewEntryNumber] = useState(null);
 	const [circulosToMap, setCirculosToMap] = useState([]);
@@ -61,64 +60,63 @@ const SubmisionForm = ({ form, submision }) => {
 	};
 
 	console.log(form.values.ciPedido);
+
 	return (
 		<div id='sub'>
-			<h3 className='text-center text-secondary '>Datos generales de la planilla</h3>
-			<h6 className='text-secondary mb-4'>Introduzca los datos de la planilla de solicitud</h6>
+			<hr />
+			<h3 className='text-center text-secondary'>Datos generales de la planilla</h3>
+			<h6 className='text-secondary mb-5'>Introduzca los datos de la planilla de solicitud</h6>
 
-			<div className='form-group '>
-				<div className='row d-flex justify-content-center'>
-					<div className='col-md-10 mb-10 '>
-						<div className='form-group '>
-							<div className='row align-items-center mt-3 mb-3'>
-								<div className='col-md-3 mb-3'>
-									<div className='om-os-group-buttons d-flex'>
-										<a className='btn' role='button' id='os' name='finality' onClick={handleOs}>
-											OS
-										</a>
-										<a className='btn ' role='button' id='om' name='finality' onClick={handleOm}>
-											OM
-										</a>
-									</div>
+			<div className='container '>
+				<div className='row justify-content-center'>
+					<div className='col-md-12 '>
+						<div className='row justify-content-evenly'>
+							<div className='col-md-3'>
+								<div className='om-os-group-buttons d-flex'>
+									<a className='btn' role='button' id='os' name='finality' onClick={handleOs}>
+										OS
+									</a>
+									<a className='btn ' role='button' id='om' name='finality' onClick={handleOm}>
+										OM
+									</a>
 								</div>
-								{/*  ****************************************************** */}
+							</div>
 
-								<div className='col-md-4 mb-4 d-flex justify-content-evenly'>
-									<div className='form-check form-check-inline'>
-										<input
-											className='form-check-input'
-											type='radio'
-											id='new'
-											name='submisiontype'
-											value='new'
-											defaultChecked
-											onChange={form.handleChange}
-											onBlur={form.handleBlur}
-										/>
-										<label className='form-check-label' htmlFor='new'>
-											New
-										</label>
-									</div>
-
-									<div className='form-check form-check-inline'>
-										<input
-											className='form-check-input'
-											type='radio'
-											id='traslado'
-											name='submisiontype'
-											value='traslado'
-											onChange={form.handleChange}
-											onBlur={form.handleBlur}
-										/>
-
-										<label className='form-check-label' htmlFor='traslado'>
-											Traslado
-										</label>
-									</div>
+							<div className='col-md-3 mb-5 d-flex align-items-center'>
+								<div className='form-check form-check-inline'>
+									<input
+										className='form-check-input'
+										type='radio'
+										id='new'
+										name='submisiontype'
+										value='new'
+										defaultChecked
+										onChange={form.handleChange}
+										onBlur={form.handleBlur}
+									/>
+									<label className='form-check-label' htmlFor='new'>
+										New
+									</label>
 								</div>
 
-								{/*  ****************************************************** */}
+								<div className='form-check form-check-inline'>
+									<input
+										className='form-check-input'
+										type='radio'
+										id='traslado'
+										name='submisiontype'
+										value='traslado'
+										onChange={form.handleChange}
+										onBlur={form.handleBlur}
+									/>
 
+									<label className='form-check-label' htmlFor='traslado'>
+										Traslado
+									</label>
+								</div>
+							</div>
+
+							<div className='col-md-3 '>
 								<InputSwitch
 									id={'socialCase'}
 									name={'socialCase'}
@@ -127,55 +125,55 @@ const SubmisionForm = ({ form, submision }) => {
 									onBlur={form.handleBlur}
 									label='Caso Social'
 								/>
+							</div>
 
-								{/*  ****************************************************** */}
-
-								<div className='col-md-3 mb-4 gap-1 d-inline-flex'>
-									<label className='m-md-2 d-inline-flex' htmlFor='entryNumber'>No. </label>
-									<input
-										className='form-control'
-										type='text'
-										id='entryNumber'
-										name='entryNumber'
-										placeholder={numberLabel}
-										disabled
-									/>
-								</div>
-
-								{/*  ****************************************************** */}
+							<div className='col-md-3 mb-5 gap-1 d-inline-flex'>
+								<label className='m-md-2 d-inline-flex' htmlFor='entryNumber'>
+									No.
+								</label>
+								<input
+									className='form-control'
+									type='text'
+									id='entryNumber'
+									name='entryNumber'
+									placeholder={numberLabel}
+									disabled
+								/>
 							</div>
 						</div>
 
-						{/*  ****************************************************** */}
+						<div className='row mb-3 justify-content-evenly'>
+							<div className='col-md-12'>
+								<Select
+									id={'ciPedido'}
+									name={'ciPedido'}
+									value={form.values.ciPedido}
+									label={'Si se solicita un circulo en particular como preferencia'}
+									optionText={'--- Seleccione el círculo que solicita ---'}
+									onChange={form.handleChange}
+									onBlur={form.handleBlur}
+									mapFunction={circulosToMap.map((circulo) => (
+										<option key={circulo._id} value={circulo.name}>
+											{circulo.name}
+										</option>
+									))}
+								/>
+							</div>
+						</div>
 
-						<Select
-							id={'ciPedido'}
-							name={'ciPedido'}
-							value={form.values.ciPedido}
-							label={'Si se solicita un circulo en particular como preferencia'}
-							optionText={'--- Seleccione el círculo que solicita ---'}
-							onChange={form.handleChange}
-							onBlur={form.handleBlur}
-							mapFunction={circulosToMap.map((circulo) => (
-								<option key={circulo._id} value={circulo.name}>
-									{circulo.name}
-								</option>
-							))}
-						/>
-
-						{/*  ****************************************************** */}
-
-						<div className='col-md-12 mb-3 mt-3'>
-							<textarea
-								className='form-control'
-								rows={2}
-								id='motive'
-								name='motive'
-								placeholder='Escriba el motivo de la solicitud...'
-								value={form.values.motive}
-								onChange={form.handleChange}
-								onBlur={form.handleBlur}
-							></textarea>
+						<div className='row mb-3 justify-content-center'>
+							<div className='col-md-12'>
+								<textarea
+									className='form-control'
+									rows={2}
+									id='motive'
+									name='motive'
+									placeholder='Escriba el motivo de la solicitud...'
+									value={form.values.motive}
+									onChange={form.handleChange}
+									onBlur={form.handleBlur}
+								></textarea>
+							</div>
 						</div>
 					</div>
 				</div>
