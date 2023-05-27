38b37e24db0c76ecea2f0a9ce3c6453216fe1f94
YABF0801 <yabf0801@gmail.com>
2023-05-27 02:52:22 -0400

    actualizar mensajes en toast en servicios de propuestas

--- src/views/Dashboard/Propuestas/service/propuestas.services.js
+++ src/views/Dashboard/Propuestas/service/propuestas.services.js
@@ -1,25 +1,40 @@
 import { baseAxios } from '../../../../api/baseAxios';
+import ErrorMsg from '../../../../common/Toasts/ErrorMsg';
+import SuccessMsg from '../../../../common/Toasts/SuccessMsg';
+import ToastNotification from '../../../../common/Toasts/toasts';
 
 export const propuestasApiGet = async () => {
 	const allPropuestas = await baseAxios.get('/submisions');
-	const propuestas = allPropuestas.data.filter(submision => submision.status === 'propuesta')
+	const propuestas = allPropuestas.data.filter((submision) => submision.status === 'propuesta');
 	return propuestas;
 };
 
 export const propuestaApiGenerar = async () => {
-    try {
-        await baseAxios.post(`/propuestas/generar`);
-    } catch (error) {
-        throw new Error(`Error al generar las propuestas : ${error.message}`);
-    }
+	try {
+		await baseAxios.post(`/propuestas/generar`);
+	} catch (error) {
+		ToastNotification('error', `${ErrorMsg('custom', 'propuestas', 'generar')} : ${error.message}`);
+	}
 };
 
-export const propuestaApiAceptar = async (submisions) => { // pasarle un arreglo 
-	const propuestas = await baseAxios.put(`/propuestas/aceptar`, submisions);
-	return propuestas.data;
+export const propuestaApiAceptar = async (submisions) => {
+	// pasarle un arreglo
+	try {
+		const propuestas = await baseAxios.put(`/propuestas/aceptar`, submisions);
+		ToastNotification('success', `${SuccessMsg('custom', `propuestas seleccionadas`, 'aceptadas')}`);
+		return propuestas.data;
+	} catch (error) {
+		ToastNotification('error', `${ErrorMsg('custom', 'propuestas', 'aceptar')} : ${error.message}`);
+	}
 };
 
-export const propuestaApiRechazar = async (submisions) => { // pasarle un arreglo 
-	const propuestas = await baseAxios.put(`/propuestas/rechazar`, submisions);
-	return propuestas.data;
+export const propuestaApiRechazar = async (submisions) => {
+	// pasarle un arreglo
+	try {
+		const propuestas = await baseAxios.put(`/propuestas/rechazar`, submisions);
+		ToastNotification('info', 'Las propuestas no seleccionadas fueron rechazadas');
+		return propuestas.data;
+	} catch (error) {
+		ToastNotification('error', `${ErrorMsg('custom', 'propuestas', 'rechazar')} : ${error.message}`);
+	}
 };
