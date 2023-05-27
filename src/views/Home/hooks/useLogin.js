480b07235332979e3c3fce77c7e4c2bdaa32c618
YABF0801 <yabf0801@gmail.com>
2023-05-27 00:27:16 -0400

    actualizar el uso de compnente toast en el formulario de login

--- src/views/Home/hooks/useLogin.js
+++ src/views/Home/hooks/useLogin.js
@@ -1,29 +1,17 @@
-import { toast } from 'react-toastify';
+import  ToastNotification  from '../../../common/Toasts/toasts';
 
 import { useAuthContext } from '../../../core/context/authContext';
 import { userApiLogin } from '../service/home.services';
 
 export const useLogin = () => {
-	const notifyError = () =>
-		toast.error('Error de credenciales!', {
-			position: 'bottom-right',
-			autoClose: 5000,
-			hideProgressBar: false,
-			closeOnClick: true,
-			pauseOnHover: true,
-			draggable: true,
-			progress: undefined,
-			theme: 'colored',
-		});
 
 	const { login } = useAuthContext();
 	const loginUser = async (userData) => {
 		try {
 			const { user, token } = await userApiLogin(userData);
-			console.log(user);
 			login(user, token);
 		} catch (error) {
-			notifyError();
+			ToastNotification('error', 'Credenciales incorrectas');  
 		}
 	};
 
