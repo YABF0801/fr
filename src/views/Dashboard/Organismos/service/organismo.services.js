6c72dfe84c701a28fe1401dac35c9ceb5b67c492
YABF0801 <yabf0801@gmail.com>
2023-05-27 02:52:07 -0400

    actualizar mensajes en toast en servicios de organismo

--- src/views/Dashboard/Organismos/service/organismo.services.js
+++ src/views/Dashboard/Organismos/service/organismo.services.js
@@ -1,4 +1,7 @@
 import { baseAxios } from '../../../../api/baseAxios';
+import ErrorMsg from '../../../../common/Toasts/ErrorMsg';
+import SuccessMsg from '../../../../common/Toasts/SuccessMsg';
+import ToastNotification from '../../../../common/Toasts/toasts';
 
 export const organismosApiGet = async () => {
 	const organismos = await baseAxios.get('/organismos/');
@@ -7,24 +10,30 @@ export const organismosApiGet = async () => {
 
 export const organismosApiCreate = async (organismo) => {
 	try {
-	const organismoCreated = await baseAxios.post('/organismos', organismo);
-	return organismoCreated.data;
-} catch (error) {
-	throw new Error(`Error al crear organismo: ${error.message}`);
-  }
+		const organismoCreated = await baseAxios.post('/organismos', organismo);
+		ToastNotification('success', `${SuccessMsg('create', `organismo ${organismo.name}`)}`);
+		return organismoCreated.data;
+	} catch (error) {
+		ToastNotification('error', `${ErrorMsg('create', 'organismo')} : ${error.message}`);
+	}
 };
 
 export const organismosApiUpdate = async (organismo) => {
 	try {
-	  await baseAxios.put(`/organismos/${organismo._id}`, organismo);
+		await baseAxios.put(`/organismos/${organismo._id}`, organismo);
+		ToastNotification('success', `${SuccessMsg('update', `organismo ${organismo.name}`)}`);
 	} catch (error) {
-	  throw new Error(`Error al actualizar organismo: ${error.response.data}`);
+		ToastNotification('error', `${ErrorMsg('update', 'organismo')} : ${error.message}`);
 	}
-  };
-
+};
 
 export const organismosApiDelete = async (id) => {
-	await baseAxios.delete(`/organismos/${id}`);
+	try {
+		await baseAxios.delete(`/organismos/${id}`);
+		ToastNotification('success', `${SuccessMsg('delete', `organismo`)}`);
+	} catch (error) {
+		ToastNotification('error', `${ErrorMsg('delete', 'organismo')} : ${error.message}`);
+	}
 };
 
 export const organismosApiFindById = async (id) => {
