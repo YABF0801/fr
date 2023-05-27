82bb7482170bd3b5c54ae8114aea7ceda915452f
YABF0801 <yabf0801@gmail.com>
2023-05-27 02:52:00 -0400

    actualizar mensajes en toast en servicios de submision

--- src/views/Dashboard/GeneralList/service/submision.services.js
+++ src/views/Dashboard/GeneralList/service/submision.services.js
@@ -1,34 +1,39 @@
-import  {baseAxios}  from '../../../../api/baseAxios';
+import { baseAxios } from '../../../../api/baseAxios';
+import ErrorMsg from '../../../../common/Toasts/ErrorMsg';
+import SuccessMsg from '../../../../common/Toasts/SuccessMsg';
 import ToastNotification from '../../../../common/Toasts/toasts';
 
 export const submisionsApiGet = async () => {
 	const allSubmisions = await baseAxios.get('/submisions');
-	const submisions = allSubmisions.data.filter(submision => submision.status !== 'propuesta')
+	const submisions = allSubmisions.data.filter((submision) => submision.status !== 'propuesta');
 	return submisions;
 };
 
 export const submisionsApiCreate = async (submision) => {
 	try {
-	const submisionCreated = await baseAxios.post('/submisions', submision);
-	return submisionCreated.data;
-} catch (error) {
-	ToastNotification('error', `Error al crear la planilla: ${error.message}`);  
-  }
+		const submisionCreated = await baseAxios.post('/submisions', submision);
+		ToastNotification('success', `${SuccessMsg('create', `planilla ${submision.child.childName}`)}`);
+		return submisionCreated.data;
+	} catch (error) {
+		ToastNotification('error', `${ErrorMsg('create', 'planilla')} : ${error.message}`);
+	}
 };
 
 export const submisionsApiUpdate = async (submision) => {
 	try {
 		await baseAxios.put(`/submisions/${submision.id}`, submision);
+		ToastNotification('success', `${SuccessMsg('update', `planilla ${submision.entryNumber}`)}`);
 	} catch (error) {
-		ToastNotification('error', `Error al actualizar la planilla: ${error.message}`);  
+		ToastNotification('error', `${ErrorMsg('update', 'planilla')} : ${error.message}`);
 	}
 };
 
 export const submisionsApiDelete = async (id) => {
 	try {
 		await baseAxios.delete(`/submisions/${id}`);
+		ToastNotification('success', `${SuccessMsg('delete', `planilla`)}`);
 	} catch (error) {
-		ToastNotification('error', `Error al eliminar la planilla: ${error.message}`);  
+		ToastNotification('error', `${ErrorMsg('delete', 'planilla')} : ${error.message}`);
 	}
 };
 
@@ -42,20 +47,20 @@ export const consecustiveApiGet = async () => {
 	return tools.data.consecutive;
 };
 
-export const consecustiveApiReset= async () => {
+export const consecustiveApiReset = async () => {
 	try {
 		await baseAxios.put(`/submisions/reset-consecutive`);
+		ToastNotification('success', `${SuccessMsg('custom', `número consecutivo`, 'reseteado')}`);
 	} catch (error) {
-		ToastNotification('error', `Error al resetear consecutivo: ${error.message}`);  
+		ToastNotification('error',  `${ErrorMsg("custom", "número consecutivo", 'resetear')} : ${error.message}`);
 	}
 };
 
 export const submisionsApiBaja = async (id) => {
 	try {
 		await baseAxios.put(`/submisions/baja/${id}`);
+		ToastNotification('success', `${SuccessMsg('custom', `baja`, 'realizada')}`);
 	} catch (error) {
-		ToastNotification('error', `Error al dar baja de la matrícula: ${error.message}`);  
+		ToastNotification('error',  `${ErrorMsg("custom", "baja", 'ejecutar')} : ${error.message}`);
 	}
-  };
-  
-
+};
