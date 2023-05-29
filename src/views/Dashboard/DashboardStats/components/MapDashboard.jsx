e8c7d3fa2a324be480345e68ca91a067e738f9e3
YABF0801 <yabf0801@gmail.com>
2023-05-29 01:41:05 -0400

    separar componenntee map paara dashboard

--- src/views/Dashboard/DashboardStats/components/MapDashboard.jsx
+++ src/views/Dashboard/DashboardStats/components/MapDashboard.jsx
@@ -1,14 +1,10 @@
-import { useEffect, useState } from 'react';
-import {circulosPositionGet,
-} from '../services/mapCirculosPosition';
-import {submisionsPositionGet,
-} from '../services/mapSubmisionsPosition';
-import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet';
-import L from 'leaflet';
+// mapa para mostrar los circulos y niños
 
-//
-// componente 3 separado de MapAndBarChart.jsxs
-//
+import { useEffect, useState } from 'react';
+import { circulosPositionGet } from '../services/mapCirculosPosition';
+import { submisionsPositionGet } from '../services/mapSubmisionsPosition';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
 
 const MapDashboard = () => {
 	const [submisionsLocal, setSubmisionsLocal] = useState([]);
@@ -42,49 +38,50 @@ const MapDashboard = () => {
 
 	return (
 
-								<MapContainer
-									className='map-container'
-									style={ { width: '100%', height: '400px' } }
-									center={ [21.72761, -82.834167] }
-									zoom={ 10 }
-									setView={ [21.72761, -82.834167] }
-									scrollWheelZoom={ true }
-									minZoom={ 9 }
-									maxBounds={ [
-										[21.410303, -83.26972], // Suroeste
-										[21.961168, -82.531547], // Noreste
-									] }
-								>
-									<TileLayer
-										attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-										url='/public/Tiles/{z}/{x}/{y}.png'
-									// https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
-									/>
+		<MapContainer
+			
+			className='map-container '
+			style={{ width: '100%', height: '100%'}}
+			center={[21.72761, -82.834167]}
+			zoom={10}
+			setView={[21.72761, -82.834167]}
+			scrollWheelZoom={true}
+			minZoom={9}
+			maxBounds={[
+				[21.410303, -83.26972], // Suroeste
+				[21.961168, -82.531547], // Noreste
+			]}
+		>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url='/public/Tiles/{z}/{x}/{y}.png'
+				// https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
+			/>
 
-									{ submisionsLocal.map((submision) => (
-										<Marker key={ submision._id } position={ submision.child.latlng } icon={ childIcon }>
-											<Popup>
-												<span className='popup'>
-													<h3>{ submision.child.childName + submision.child.childLastname }</h3>
-													<p>{ submision.child.childAddress }</p>
-													<p>
-														Edad: { submision.child.age }, { submision.child.sex }
-													</p>
-												</span>
-											</Popup>
-										</Marker>
-									)) }
+			{submisionsLocal.map((submision) => (
+				<Marker key={submision._id} position={submision.child.latlng} icon={childIcon}>
+					<Popup>
+						<span className='popup'>
+							<h3>{submision.child.childName + submision.child.childLastname}</h3>
+							<p>{submision.child.childAddress}</p>
+							<p>
+								Edad: {submision.child.age}, {submision.child.sex}
+							</p>
+						</span>
+					</Popup>
+				</Marker>
+			))}
 
-									{ circulosLocal.map((circulo) => (
-										<Marker key={ circulo._id } position={ circulo.latlng } icon={ ciIcon }>
-											<Popup>
-												<div>
-													<p>CI { circulo.name }</p>
-												</div>
-											</Popup>
-										</Marker>
-									)) }
-								</MapContainer>
+			{circulosLocal.map((circulo) => (
+				<Marker key={circulo._id} position={circulo.latlng} icon={ciIcon}>
+					<Popup>
+						<div>
+							<p>CI {circulo.name}</p>
+						</div>
+					</Popup>
+				</Marker>
+			))}
+		</MapContainer>
 
 	);
 };
