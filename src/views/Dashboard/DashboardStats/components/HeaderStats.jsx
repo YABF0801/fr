559215dadb9ebf1ebe83c9a8354f2073d18a63f5
YABF0801 <yabf0801@gmail.com>
2023-05-29 01:40:15 -0400

    refactor header dash commponente

--- src/views/Dashboard/DashboardStats/components/HeaderStats.jsx
+++ src/views/Dashboard/DashboardStats/components/HeaderStats.jsx
@@ -1,16 +1,52 @@
+import { useEffect, useState } from 'react';
+import { useAuthContext } from '../../../../core/context/authContext';
+import { FechaOmApiGet } from '../../../../utils/utiles.sevices';
+import { cursoApiGet } from '../../Circulos/service/circulo.services';
+
 const HeaderStats = () => {
-  return (
+	const [curso, setCurso] = useState();
+	const [date, setDate] = useState(false);
+	const [existingDate, setExistingDate] = useState(false);
+	const { isAuthenticated } = useAuthContext();
 
-    <div className='row'>
-      <div className='col-md-4 '>
-        <h3 className='text-secondary '>Fecha de nuevo otorgamiento masivo { dateShow }</h3>
-      </div>
-      <div className='col-md-6 '></div>
-      <div className='col-md-2'>
-        <h2 className='text-center '>Curso { curso }</h2>
-      </div>
-    </div>
+	const dateShow = existingDate && new Date(date).toLocaleDateString();
 
-  )
-}
-export default HeaderStats
+	useEffect(() => {
+		const fetchData = async () => {
+			const curso = await cursoApiGet();
+			setCurso(curso);
+		};
+		fetchData();
+	}, []);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const omDate = await FechaOmApiGet();
+			if (omDate) {
+				setDate(omDate);
+				setExistingDate(true);
+			}
+		};
+		fetchData();
+	}, []);
+
+	return (
+		<div className='row mt-3 justify-content-between'>
+			<div className='col-md-5'>
+			
+				{dateShow ? (
+					<h5 className='text-secondary'>Fecha de nuevo otorgamiento masivo {dateShow}</h5>
+				) : isAuthenticated.user?.role === 'admin' ? (
+					<h5 className='text-secondary'>Establezca fecha de nuevo otorgamiento masivo</h5>
+				) : (
+					null
+				)}
+			</div>
+
+			<div className='col-md-2'>
+				<h2 className='text-center '>Curso {curso}</h2>
+			</div>
+		</div>
+	);
+};
+export default HeaderStats;
