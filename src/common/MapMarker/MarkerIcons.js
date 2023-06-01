04b9effb5c608c4b920c7f2f9cad225c29af2dec
YABF0801 <yabf0801@gmail.com>
2023-05-31 23:36:13 -0400

    map dashboard hooks and markers

---
+++ src/common/MapMarker/MarkerIcons.js
@@ -0,0 +1,18 @@
+import L from 'leaflet';
+   
+export const childIcon = L.icon({
+		iconUrl: '/public/kid2.png',
+		iconSize: [32, 32],
+		iconAnchor: [16, 32],
+		popupAnchor: [0, -32],
+		shadowAnchor: [4, 62],
+	});
+
+export const ciIcon = L.icon({
+		iconUrl: '/public/ci.png',
+		iconSize: [32, 32],
+		iconAnchor: [16, 32],
+		popupAnchor: [0, -32],
+		shadowAnchor: [4, 62],
+	});
+
