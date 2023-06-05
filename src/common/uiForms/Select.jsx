35347b0a9ea5e3c647342cfb16f6e424474af762
YABF0801 <yabf0801@gmail.com>
2023-06-05 01:16:33 -0400

    select update

--- src/common/uiForms/Select.jsx
+++ src/common/uiForms/Select.jsx
@@ -1,28 +1,33 @@
-const Select = ({  className, id, name, value, label, optionText, onChange, onBlur, disabled, mapFunction}) => {
-	return (
-		<div className={ className || 'form-group col-md-12 mb-3 d-flex' }>
-			{ label && (
-				<label className='col-md-6 m-md-2 text-secondary' htmlFor={ id }>
-					{ label }
-				</label>
-			) }
+import { Form } from 'react-bootstrap';
 
-			<select
-				className='form-control'
-				id={ id }
-				name={ name }
-				value={ value }
-				onChange={ onChange }
-				onBlur={ onBlur }
-				disabled={ disabled || false }
-			>
-				<option className='text-center' value='0'>{ optionText }</option>
-				{ mapFunction }
-			</select>
-		</div>
-	);
+const Select = ({ className, id, name, value, label, optionText, onChange, onBlur, disabled, mapFunction }) => {
+
+  return (
+    <div className={className || 'form-group col-md-12 mb-3 d-flex'}>
+      {label && (
+        <Form.Label className='col-md-6 m-md-2 text-secondary' htmlFor={id}>
+          {label}
+        </Form.Label>
+      )}
+
+      <Form.Select
+        className='form-control'
+        id={id}
+        name={name}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        disabled={disabled || false}
+      >
+        <option className='text-center' value='0'>
+          {optionText}
+        </option>
+        {mapFunction}
+      </Form.Select>
+	  
+    </div>
+  );
 };
 
 export default Select;
-  
-  
+
