34ed47461d8bee5d25951ee27166091d6f320bd4
YABF0801 <yabf0801@gmail.com>
2023-06-19 09:39:05 -0400

    export btn

--- src/common/ExportBtn.jsx
+++ src/common/ExportBtn.jsx
@@ -1,19 +1,21 @@
-import Dropdown from 'react-bootstrap/Dropdown';
+// import Dropdown from 'react-bootstrap/Dropdown';
 
 function ExportBtn({ handleExport , exportPage}) {
 
 	return (
 		<>
-			<Dropdown>
-				<Dropdown.Toggle className='btn export-btn' id='dropdown-export' drop='down-centered'>
+				<button className='btn export-btn' onClick={handleExport}>
 					Exportar
-				</Dropdown.Toggle>
+				</button>
 
+{/* 
+			<Dropdown>
+				
 				<Dropdown.Menu>
 					<Dropdown.Item onClick={exportPage}>Página actual</Dropdown.Item>
 					<Dropdown.Item onClick={handleExport}>Todo</Dropdown.Item>
 				</Dropdown.Menu>
-			</Dropdown>						
+			</Dropdown>						 */}
 
 		</>
 	);
